8e16365dc0148483bb89b0bd05c4ba2dca57aef6
marcio.v.ordonez@gmail.com <marcio.v.ordonez@gmail.com>
2020-04-26 12:10:15 -0300

    variaveis de ambiente

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-const token = '';
+const token = process.env.Token || '';
 const compliments = ['incrivel', 'linda', 'maravilhosa', 'D+', 'Muito legal', 'Que nunca votaria no babu'];
 
 bot.on('ready', () =>{
