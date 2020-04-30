7f177363ce11547b331ec6cf00fad95959321da0
marcio.v.ordonez@gmail.com <marcio.v.ordonez@gmail.com>
2020-04-29 21:02:09 -0300

    pause stop resume music

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const token = process.env.TOKEN || '';
 const compliments = ['incrivel', 'linda', 'maravilhosa', 'D+', 'Muito legal', 'Que nunca votaria no babu'];
 const doggy = 'https://dog.ceo/api/breeds/image/random';
 const catty = 'https://api.thecatapi.com/v1/images/search';
-const queue = ['https://www.youtube.com/watch?v=jkJ5q2NxHf4'];
-
+const queue = ['https://www.youtube.com/watch?v=jkJ5q2NxHf4','https://www.youtube.com/watch?v=r9tBBvvLkd8'];
+var volume = 0.5;
 bot.on('ready', () =>{
     console.log('Bot is running! ');
 });
@@ -18,10 +18,10 @@ bot.on('ready', () =>{
 
 
 bot.on('message', msg =>{
-    if (msg.content === "Estou triste"){
+    if (msg.content.toLowerCase().includes("estou triste")){
         msg.reply('Não fique triste voce é uma pessoa '+compliments[Math.floor(Math.random()*compliments.length)]);
     }
-    if (msg.content === 'AU'){
+    if (msg.content.includes('AU')){
         request(doggy, {json: true}, (err, res, body)=>{
             if(err){
                 return msg.reply('Deu erro :(');
@@ -29,7 +29,7 @@ bot.on('message', msg =>{
             return msg.reply(body.message);
         })
     }
-    if (msg.content === 'MIAU'){
+    if (msg.content.includes('MIAU')){
         request(catty, {json: true}, (err, res, body)=>{
             if(err){
                 return msg.reply('Deu erro :(');
@@ -42,15 +42,36 @@ bot.on('message', msg =>{
         switch(args[1]){
             case 'play':
                 function start(connection, msg){
-                    connection.play(ytdl(queue[0], {filter:'audioonly'}));
+                    queue.dispatcher = connection.play(ytdl(queue[0], {filter:'audioonly'}));
+                    // return connection.play(ytdl(queue[0], {filter:'audioonly'}));
+                    return;
                 }
 
                 if(!msg.member.voice.channel){
                     return msg.reply('Voce precisa estar em um canal pra festa poder comerçar rs');
                 }
                 if(!msg.guild.voiceConnection) msg.member.voice.channel.join().then( (connection)=>{
+                    // var dispatcher = start(connection, msg);
                     start(connection, msg);
+                    queue.dispatcher.setVolume(volume);
                 })
+            break;
+            case 'stop':
+                if(queue.dispatcher) queue.dispatcher.destroy();
+            break;
+            case 'pause':
+                if(queue.dispatcher) queue.dispatcher.pause();
+            break;
+            case 'resume':
+                if(queue.dispatcher) queue.dispatcher.resume();
+            break;
+            case '+':
+                if(volume < 90) volume = volume + 0.10;
+                queue.dispatcher.setVolume(volume);
+            break;
+            case '-':
+                if(volume > 10 )volume = volume - 0.10;
+                queue.dispatcher.setVolume(volume);
             break;
         }
     }
